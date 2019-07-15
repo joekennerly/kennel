5d832b1bfe8cb3439dcaf2524c55ac85d1bd2bba
joekennerly <joseph.kennerly@gmail.com>
2019-07-15 11:21:40 -0500

    move data from appviews to api

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,55 +6,31 @@ import EmployeeList from "./employee/EmployeeList";
 import OwnerList from "./owner/OwnerList";
 
 export default class ApplicationViews extends Component {
-  employeesFromAPI = [
-    { id: 1, name: "Jessica Younker" },
-    { id: 2, name: "Jordan Nelson" },
-    { id: 3, name: "Zoe LeBlanc" },
-    { id: 4, name: "Blaise Roberts" }
-  ];
-
-  locationsFromAPI = [
-    { id: 1, name: "Nashville North", address: "500 Circle Way" },
-    { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-  ];
-
-  animalsFromAPI = [
-    { id: 1, name: "Doodles" },
-    { id: 2, name: "Jack" },
-    { id: 3, name: "Angus" },
-    { id: 4, name: "Henley" },
-    { id: 5, name: "Derkins" },
-    { id: 6, name: "Checkers" }
-  ];
-
-  ownersFromAPI = [
-    { id: 1, name: "Ryan Tanay", phoneNumber: "555-555-5555" },
-    { id: 2, name: "Emma Beaton", phoneNumber: "555-555-5555" },
-    { id: 3, name: "Dani Adkins", phoneNumber: "555-555-5555" },
-    { id: 4, name: "Adam Oswalt", phoneNumber: "555-555-5555" },
-    { id: 5, name: "Fletcher Bangs", phoneNumber: "555-555-5555" },
-    { id: 6, name: "Angela Lee", phoneNumber: "555-555-5555" }
-  ]
-
-  animalOwners = [
-    { id: 1, animal_Id: 1, owner_Id: 1},
-    { id: 2, animal_Id: 1, owner_Id: 2},
-    { id: 3, animal_Id: 2, owner_Id: 3},
-    { id: 4, animal_Id: 3, owner_Id: 1},
-    { id: 5, animal_Id: 3, owner_Id: 2},
-    { id: 6, animal_Id: 4, owner_Id: 4},
-    { id: 7, animal_Id: 5, owner_Id: 4},
-    { id: 8, animal_Id: 5, owner_Id: 5},
-    { id: 9, animal_Id: 6, owner_Id: 6}
-  ]
-
   state = {
-    owners: this.ownersFromAPI,
-    employees: this.employeesFromAPI,
-    locations: this.locationsFromAPI,
-    animals: this.animalsFromAPI,
-    animalOwners: this.animalOwners
+    owners: [],
+    employees: [],
+    locations: [],
+    animals: [],
+    animalOwners: []
   };
+
+  componentDidMount() {
+    const newState = {}
+
+    fetch("http://localhost:5002/animals")
+      .then(r => r.json())
+      .then(animals => newState.animals = animals)
+      .then(() => fetch("http://localhost:5002/employees")
+      .then(r => r.json()))
+      .then(employees => newState.employees = employees)
+      .then(() => fetch("http://localhost:5002/locations")
+      .then(r => r.json()))
+      .then(locations => newState.locations = locations)
+      .then(() => fetch("http://localhost:5002/owners")
+      .then(r => r.json()))
+      .then(owners => newState.owners = owners)
+      .then(() => this.setState(newState))
+  }
 
   render() {
     return (
